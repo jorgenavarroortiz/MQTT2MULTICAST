4b173051dd334f0ff6fc34503d42c48fb2e7980c
jorgenavarroortiz <jorgenavarro@ugr.es>
2021-12-29 19:46:56 +0100

    Add shortest path to unicast routing, testing

--- RYU/ryu/app/simple_switch_13_MQTT2MULTICAST.py
+++ RYU/ryu/app/simple_switch_13_MQTT2MULTICAST.py
@@ -18,11 +18,12 @@
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
+from ryu.lib.packet import arp
 from ryu.lib.packet import ether_types
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import in_proto
@@ -30,8 +31,19 @@ from ryu.lib.packet import udp
 from ryu.lib import addrconv
 from ryu.lib.packet import mqtt2multicast
 from datetime import datetime
-
 import struct, socket
+
+from ryu.topology import event
+# Below is the library used for topo discovery
+# Taken information from https://github.com/Ehsan70/RyuApps/blob/master/TopoDiscoveryInRyu.md
+# Taken information from https://github.com/castroflavio/ryu/blob/master/ryu/app/shortestpath.py
+# Taken information from https://github.com/YanHaoChen/Learning-SDN/blob/master/Controller/Ryu/ShortestPath/shortest_path_with_networkx.py
+from ryu.topology.api import get_switch, get_link
+import copy
+import networkx as nx
+import time
+
+LEARNING_SWITCH = False # True for learning switch. If False, shortest path will be employed.
 
 
 class SimpleSwitch13(app_manager.RyuApp):
@@ -44,8 +56,20 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.mac_addr = '11:22:33:44:55:66'
         self.ip_addr  = '192.168.1.100'
         self.topicToMulticast = {}
+        self.multicastTransmitterForTopic = {}
+        self.multicastTransmitterForTopicLastTimeSeen = {}
+        self.multicastTransmitterTimeout = 3600 # If an MQTT publisher is not seen after this time, it should be removed from self.multicastTransmitterForTopic.
         self.multicastReceiverForTopic = {}
         self.firstMulticastIPAddress = '225.0.0.0'
+
+        # Holds the topology data and structure
+        self.topology_api_app = self
+        self.net=nx.DiGraph()
+        self.switch_map = {}
+
+    # Handy function that lists all attributes in the given object
+    def ls(self,obj):
+        print("\n".join([x for x in dir(obj) if x[0] != "_"]))
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -53,13 +77,13 @@ class SimpleSwitch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
+        # For shortest path
+        self.switch_map.update({datapath.id: datapath})	
+
         # install table-miss flow entry
         #
-        # We specify NO BUFFER to max_len of the output action due to
-        # OVS bug. At this moment, if we specify a lesser number, e.g.,
-        # 128, OVS will send Packet-In with invalid buffer_id and
-        # truncated packet data. In that case, we cannot output packets
-        # correctly.  The bug has been fixed in OVS v2.1.0.
+        # We specify NO BUFFER to max_len of the output action due to OVS bug. At this moment, if we specify a lesser number, e.g., 128, OVS will send Packet-In 
+        # with invalid buffer_id and truncated packet data. In that case, we cannot output packets correctly.  The bug has been fixed in OVS v2.1.0.
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
@@ -88,8 +112,7 @@ class SimpleSwitch13(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        # If you hit this you might want to increase
-        # the "miss_send_length" of your switch
+        # If you hit this you might want to increase the "miss_send_length" of your switch
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
@@ -101,15 +124,33 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
             return
 
 
-        ##############
+        #################################################
+        # Add host/switch to topology if not included yet
+        #################################################
+	# forwarded by shortest path
+        pkt_arp = pkt.get_protocol(arp.arp)
+        if True: #if pkt_arp:
+            if not self.net.has_node(eth.src):
+                self.logger.info("### [TOPOLOGY] Add %s in self.net (switch %d, port %d)", eth.src, datapath.id, in_port)
+                self.net.add_node(eth.src)
+                self.net.add_edge(eth.src, datapath.id)
+                self.net.add_edge(datapath.id, eth.src, port=in_port)
+                self.logger.info("### [TOPOLOGY] List of nodes: %s", self.net.nodes())
+                self.logger.info("### [TOPOLOGY] List of links: %s", self.net.edges())
+
+
+        ################################
         # Handling UDP packets (over IP)
-        ##############
+        ################################
         pkt_ipv4 = pkt.get_protocol(ipv4.ipv4)
         self.logger.info("### pkt_ipv4: %s", pkt_ipv4)
         if pkt_ipv4:
@@ -141,48 +182,103 @@ class SimpleSwitch13(app_manager.RyuApp):
                             return
 
 
-        # The previous handler will exit this function if required.
-        # If not, the learning switch has to be executed for the current packet.
+        # The previous handler will exit this function if required. If not, the learning switch has to be executed for the current packet.
 
 
         ###################
 	### Learning switch
         ###################
-        dst = eth.dst
-        src = eth.src
+        if LEARNING_SWITCH:
+            self.mac_to_port.setdefault(dpid, {})
 
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
+            self.logger.info("[LEARNING SWITCH] packet in dpid=%s, src=%s, dst=%s, in_port=%s", dpid, src, dst, in_port)
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+            # learn a mac address to avoid FLOOD next time.
+            self.mac_to_port[dpid][src] = in_port
 
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
-
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
-
-        actions = [parser.OFPActionOutput(out_port)]
-
-        # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            # verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
+            if dst in self.mac_to_port[dpid]:
+                out_port = self.mac_to_port[dpid][dst]
+                self.logger.info("[LEARNING SWITCH] Send packet in switch %s to port %s", dpid, out_port)
             else:
-                self.add_flow(datapath, 1, match, actions)
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
+                out_port = ofproto.OFPP_FLOOD
+                self.logger.info("[LEARNING SWITCH] Send packet in switch %s to all ports (flooding)", dpid)
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
+            actions = [parser.OFPActionOutput(out_port)]
+
+            # install a flow to avoid packet_in next time
+            if out_port != ofproto.OFPP_FLOOD:
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+                # verify if we have a valid buffer_id, if yes avoid to send both flow_mod & packet_out
+                if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                    self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                    return
+                else:
+                    self.add_flow(datapath, 1, match, actions)
+            data = None
+            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                data = msg.data
+
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                      in_port=in_port, actions=actions, data=data)
+            datapath.send_msg(out)
+
+        # Test shortest path
+#        if self.net.has_node(eth.dst):
+#            self.logger.info("[SHORTEST PATH] %s in self.net", eth.dst)
+#            self.logger.info("[SHORTEST PATH] Topology - nodes: %s", self.net.nodes())
+#            self.logger.info("[SHORTEST PATH] Topology - links: %s", self.net.edges())
+#            path = nx.shortest_path(self.net, eth.src, eth.dst)
+#            self.logger.info("[SHORTEST PATH] shortest path from %s to %s: %s", eth.src, eth.dst, path)
+
+        else:
+            self.logger.info("[SHORTEST PATH] packet in dpid=%s (in_port=%s), src=%s, dst=%s", dpid, in_port, src, dst)
+            if self.net.has_node(eth.dst):
+                #self.logger.info("%s in self.net", eth.dst)
+                path = nx.shortest_path(self.net, eth.src, eth.dst)
+                self.logger.info("[SHORTEST PATH] Shortest path from src=%s to dst=%s: %s", src, dst, path)
+                next_match = parser.OFPMatch(eth_dst=eth.dst)
+                back_match = parser.OFPMatch(eth_dst=eth.src)
+                #self.logger.info("path: %s", path)
+                for on_path_switch in range(1, len(path)-1):
+                     now_switch = path[on_path_switch]
+                     next_switch = path[on_path_switch+1]
+                     back_switch = path[on_path_switch-1]
+                     next_port = self.net[now_switch][next_switch]['port']
+                     back_port = self.net[now_switch][back_switch]['port']
+                     actions = [parser.OFPActionOutput(next_port)]
+                     if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                         self.add_flow(self.switch_map[now_switch], 1, next_match, actions, msg.buffer_id)
+                     else:
+                         self.add_flow(self.switch_map[now_switch], 1, next_match, actions)
+                     actions = [parser.OFPActionOutput(next_port)]
+                     if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                         self.add_flow(self.switch_map[now_switch], 1, next_match, actions, msg.buffer_id)
+                     else:
+                         self.add_flow(self.switch_map[now_switch], 1, next_match, actions)
+
+            else:
+                out_port = ofproto.OFPP_FLOOD
+                self.logger.info("[SHORTEST PATH] Send packet in switch %s to all ports (flooding)", dpid)
+
+                actions = [parser.OFPActionOutput(out_port)]
+
+                # install a flow to avoid packet_in next time
+                if out_port != ofproto.OFPP_FLOOD:
+                    match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+                    # verify if we have a valid buffer_id, if yes avoid to send both flow_mod & packet_out
+                    if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                        self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                        return
+                    else:
+                        self.add_flow(datapath, 1, match, actions)
+                data = None
+                if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                    data = msg.data
+
+                out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                          in_port=in_port, actions=actions, data=data)
+                datapath.send_msg(out)
+
 
     #########################
     ### MQTT to MULTICAST APP
@@ -213,7 +309,18 @@ class SimpleSwitch13(app_manager.RyuApp):
                 self.logger.info("### %s > MQTT2MULTICAST - multicast IP address assigned to new topic ('%s'): %s", now, topic.decode(), multicastIPAddress)
                 self.topicToMulticast[topic] = multicastIPAddress
 
-            if flags == 1:
+            if flags == 0:
+                # The sender is going to publish to this multicast IP address
+                self.logger.info("### %s > MQTT2MULTICAST - %s will publish to the multicast IP address %s", now, pkt_ipv4.src, multicastIPAddress)
+                if topic.decode() in self.multicastTransmitterForTopic:
+                    multicastTransmitterForThisTopic = self.multicastTransmitterForTopic[topic.decode()]
+                    multicastTransmitterForThisTopic.append(pkt_ipv4.src)
+                else:
+                    self.multicastTransmitterForTopic[topic.decode()] = [pkt_ipv4.src]
+                self.multicastTransmitterForTopicLastTimeSeen[pkt_ipv4.src + ' - ' + topic.decode()] = now
+                self.logger.info("### %s > MQTT2MULTICAST - multicast transmitters for topic %s: %s", now, topic.decode(), self.multicastTransmitterForTopic[topic.decode()])
+
+            elif flags == 1:
                 # The sender subscribes to this multicast IP address
                 self.logger.info("### %s > MQTT2MULTICAST - subscribe %s to the multicast IP address %s", now, pkt_ipv4.src, multicastIPAddress)
                 if topic.decode() in self.multicastReceiverForTopic:
@@ -221,7 +328,7 @@ class SimpleSwitch13(app_manager.RyuApp):
                     multicastReceiverForThisTopic.append(pkt_ipv4.src)
                 else:
                     self.multicastReceiverForTopic[topic.decode()] = [pkt_ipv4.src]
-                self.logger.info("### %s > MQTT2MULTICAST - multicast receivers for topic %s: %s", now, topic.decode(), self.multicastReceiverForTopic[topic.decode()])                    
+                self.logger.info("### %s > MQTT2MULTICAST - multicast receivers for topic %s: %s", now, topic.decode(), self.multicastReceiverForTopic[topic.decode()])
 
             elif flags == 2:
                 # The sender unsubscribes to this multicast IP address
@@ -274,7 +381,6 @@ class SimpleSwitch13(app_manager.RyuApp):
             return
 
     def _send_packet(self, datapath, port, pkt):
-
         # Send packet
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -286,9 +392,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         datapath.send_msg(out)
 
     def _get_nth_multicast_ip_address(self, n):
-
         # n starts at 0, i.e. the first multicast IP address would be self._get_nth_multicast_ip_address(0)
-
         (forthByte, thirdByte, secondByte, firstByte) = struct.unpack('BBBB', socket.inet_aton(self.firstMulticastIPAddress))
         #self.logger.info("### multicastIPAddress: %s.%s.%s.%s", forthByte, thirdByte, secondByte, firstByte)
 
@@ -305,4 +409,50 @@ class SimpleSwitch13(app_manager.RyuApp):
         multicastIPAddress = str(auxForthByte) + '.' + str(auxThirdByte) + '.' + str(auxSecondByte) + '.' + str(auxFirstByte)
 
         return multicastIPAddress
+
+    ###################################################################################
+    """
+    The event EventSwitchEnter will trigger the activation of get_topology_data().
+    """
+    @set_ev_cls(event.EventSwitchEnter)
+    def get_topology_data(self, ev):
+
+        time.sleep(1.0) # Some time is required or some links may not appear.
+
+        # The Function get_switch(self, None) outputs the list of switches.
+        switch_list = copy.copy(get_switch(self.topology_api_app, None))
+        # For networkx
+        switches =[switch.dp.id for switch in switch_list]
+        self.net.add_nodes_from(switches)
+        self.logger.info("### [TOPOLOGY] List of nodes: %s", self.net.nodes())
+
+        # The Function get_link(self, None) outputs the list of links.
+        links_list = copy.copy(get_link(self.topology_api_app, None))
+        # For networkx
+        links=[(link.src.dpid,link.dst.dpid,{'port':link.src.port_no}) for link in links_list]
+        self.net.add_edges_from(links)
+        links=[(link.dst.dpid,link.src.dpid,{'port':link.dst.port_no}) for link in links_list]
+        self.net.add_edges_from(links)
+        self.logger.info("### [TOPOLOGY] List of links: %s", self.net.edges())
+
+
+        """
+        Now you have saved the links and switches of the topo. So you could do all sort of stuf with them. 
+        """
+
+#        print(" \t" + "Current Links:")
+#        for l in switch_list:
+#            print (" \t\t" + str(l))
+
+#        print(" \t" + "Current Switches:")
+#        for s in links_list:
+#            print (" \t\t" + str(s))
+
+    """
+    This event is fired when a switch leaves the topo. i.e. fails.
+    """
+    @set_ev_cls(event.EventSwitchLeave, [MAIN_DISPATCHER, CONFIG_DISPATCHER, DEAD_DISPATCHER])
+    def handler_switch_leave(self, ev):
+        self.logger.info("Not tracking Switches, switch leaved.")
+
 
