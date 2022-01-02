737b4df652fcec3a0ae301949c44b4825b9f1380
jorgenavarroortiz <jorgenavarro@ugr.es>
2022-01-02 16:02:38 +0100

    Generalized tree topology, with possibility of adding real network interface to a host or switch, and to add delay to links between switches or links to hosts

--- mininet/topo_mqtt_lora_VM_bridged.py
+++ mininet/topo_mqtt_lora_VM_bridged.py
@@ -22,7 +22,10 @@ delayLinkHost = '0ms'
 delayLinkSwitches = '0ms'
 fanoutPerLevel = [2,2] # Last fanout is for hosts
 realNetworkInterface = None
-hostToConnectRealNetworkInterface = None
+elementToConnectRealNetworkInterface = None
+RH = 0 # 1 if a real network interface is connected to a host
+RS = 0 # 1 if a real network interface is connected to a switch
+numberElement = 0 # Host or switch connected using a real network interface
 
 
 def checkIntf( intf ):
@@ -109,6 +112,12 @@ def myNetwork():
 		if verbose: print("Link %d: %s - %s" % (i, linksU[i], linksB[i]))
 
 
+	# Check if element (host or switch) with a real network interface exists
+	if (RH == 1 and numberElement > noHosts) or (RS == 1 and numberElement > noSwitches):
+		print("Host %s does not exist. Exiting." % (elementToConnectRealNetworkInterface))
+		sys.exit()
+
+
 	# Create hosts
 	info( '*** Add hosts\n' )
 	hosts = []
@@ -126,11 +135,11 @@ def myNetwork():
 
 	# "Real" network interface connected to host
 	if realNetworkInterface is not None:
-		print("*** Add %s to %s" % (realNetworkInterface, hostToConnectRealNetworkInterface))
+		print("*** Add %s to %s" % (realNetworkInterface, elementToConnectRealNetworkInterface))
 		intfName=realNetworkInterface
-		info( '*** Checking', intfName, '\n' )
+#		info( '*** Checking', intfName, '\n' )
 #		checkIntf( intfName )                               # Not required, just for checking if the real network interface is in use...
-		Intf( intfName, node=net.get(hostToConnectRealNetworkInterface) )
+		Intf( intfName, node=net.get(elementToConnectRealNetworkInterface) )
 
 	# Create links
 	info( '*** Add links\n')
@@ -180,25 +189,18 @@ def myNetwork():
 		sw.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
 
 	# "Real" network interface connected to host
-	if realNetworkInterface is not None:
+	if RH == 1:
 		# Adjust the IP addresses of this host. The real network interface will be in the 192.168.2.0/24 network, and the hX-eth1 will be in the 192.168.1.0/24 network (like the rest of the hosts).
-		numberHostStr=unicode(hostToConnectRealNetworkInterface[-1:], 'utf-8')
-		if numberHostStr.isnumeric():
-			numberHost = int(numberHostStr)
-			# Configure the IP address of the real network interface and the XX-eth0 interface
-			if numberHost < 10:
-				commandStr1 = 'ifconfig ' + realNetworkInterface + ' 192.168.2.10' + numberHostStr + '/24'
-				commandStr2 = 'ifconfig ' + hostToConnectRealNetworkInterface + '-eth1 192.168.1.10' + numberHostStr + '/24'
-			else:
-				commandStr1 = 'ifconfig ' + realNetworkInterface + ' 192.168.2.1' + numberHostStr + '/24'
-				commandStr2 = 'ifconfig ' + hostToConnectRealNetworkInterface + '-eth1 192.168.1.1' + numberHostStr + '/24'
-			net.get(hostToConnectRealNetworkInterface).cmd(commandStr1)
-			net.get(hostToConnectRealNetworkInterface).cmd(commandStr2)
-			net.get(hostToConnectRealNetworkInterface).cmd("sysctl -w net.ipv4.ip_forward=1")
+		lastByteIPAddress = str(100 + numberElement)
+		commandStr1 = 'ifconfig ' + realNetworkInterface + ' 192.168.2.' + lastByteIPAddress + '/24'
+		commandStr2 = 'ifconfig ' + elementToConnectRealNetworkInterface + '-eth1 192.168.1.' + lastByteIPAddress + '/24'
+		print("*** Configure host %s: ip_forward=1, real network interface %s with IP address %s and interface %s-eth1 with IP address %s" % (elementToConnectRealNetworkInterface, realNetworkInterface, '192.168.2.' + lastByteIPAddress, elementToConnectRealNetworkInterface, '192.168.1.' + lastByteIPAddress))
+		net.get(elementToConnectRealNetworkInterface).cmd(commandStr1)
+		net.get(elementToConnectRealNetworkInterface).cmd(commandStr2)
+		net.get(elementToConnectRealNetworkInterface).cmd("sysctl -w net.ipv4.ip_forward=1")
 
-		else:
-			print("Error parsing host name. Exiting.")
-			sys.exit()
+	elif RS == 1:
+		print("*** Nothing to configure in switch %s with real network interface" % (elementToConnectRealNetworkInterface))
 
 	# Open Mininet Command Line Interface
 	CLI(net)
@@ -213,7 +215,7 @@ def usage():
 	print("         %s -v -f 2 -f 3 -f 2 -d 10ms" % (sys.argv[0]))
 
 def main():
-	global delayLinkHost, delayLinkSwitches, fanoutPerLevel, realNetworkInterface, hostToConnectRealNetworkInterface
+	global delayLinkHost, delayLinkSwitches, fanoutPerLevel, realNetworkInterface, elementToConnectRealNetworkInterface, RH, RS, numberElement
 
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "hf:d:D:r:R:v", ["help", "output="])
@@ -252,11 +254,26 @@ def main():
 			r = 1
 			realNetworkInterface = a
 			print("Real network interface: %s" % (realNetworkInterface))
-		elif o in ("-R", "--hostToConnectRealNetworkInterface"):
+		elif o in ("-R", "--elementToConnectRealNetworkInterface"):
 			R = 1
-			hostToConnectRealNetworkInterface = a
-			print("Host to connect real network interface: %s" % (hostToConnectRealNetworkInterface))
-			print("This host will act as a router (ip_forward = 1).")
+			elementToConnectRealNetworkInterface = a
+			if elementToConnectRealNetworkInterface[0] == 'h':
+				RH = 1
+				print("This host (%s) will connect using a real network interface and will act as a router (ip_forward = 1)" % (elementToConnectRealNetworkInterface))
+			elif elementToConnectRealNetworkInterface[0] == 's':
+				RS = 1
+				print("This switch (%s) will connect using a real network interface" % (elementToConnectRealNetworkInterface))
+			else:
+				print("A switch or a host have to be selected to connect using a real network interface. Exiting.")
+				sys.exit()
+
+			numberElementStr=unicode(elementToConnectRealNetworkInterface[-1:], 'utf-8')
+			if not numberElementStr.isnumeric():
+				print("The format of the element is not hX or sX (X being an integer). Exiting.")
+				sys.exit()
+			else:
+				numberElement = int(numberElementStr)
+
 		else:
 			assert False, "unhandled option"
 
